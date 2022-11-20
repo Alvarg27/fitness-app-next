4365fc5818809b15357a494d797604ef510e1d11
Alvarg27 <alvargbesne@gmail.com>
2022-11-20 17:00:16 -0600

    Update ExerciseSearchHitCard.js

--- components/ExerciseSearchHitCard.js
+++ components/ExerciseSearchHitCard.js
@@ -18,31 +18,49 @@ const ExerciseSearchHitCard = ({
       }}
       onMouseOver={() => setIsHovered(true)}
       onMouseOut={() => setIsHovered(false)}
-      className="flex m-2 hover:bg-blue-100 transition duration-300 rounded-lg cursor-pointer"
+      className="flex m-2 hover:bg-blue-100 transition duration-300 rounded-lg cursor-pointer justify-between"
     >
-      <div className="aspect-square h-[50px] w-[50px] relative my-auto mr-4 cursor-pointer  rounded-lg bg-gray-200">
-        {hit.poster && (
-          <Image
-            src={hit.poster}
-            fill={true}
-            className="object-cover rounded-lg"
-            alt=""
-          />
-        )}
-        {!hit.poster && (
-          <div className="w-full h-full flex">
-            <FaDumbbell className="m-auto text-gray-300 text-xl" />
-          </div>
-        )}
-      </div>
+      <div className="flex">
+        <div className="aspect-square h-[40px] w-[40px] relative my-auto mr-4 cursor-pointer  rounded-lg bg-gray-200">
+          {hit.poster && (
+            <Image
+              src={hit.poster}
+              fill={true}
+              className="object-cover rounded-lg"
+              alt=""
+            />
+          )}
+          {!hit.poster && (
+            <div className="w-full h-full flex">
+              <FaDumbbell className="m-auto text-gray-300 text-xl" />
+            </div>
+          )}
+        </div>
 
-      <p
-        className={`my-auto font-medium transition duration-300 ${
-          isHovered ? "text-blue-500" : ""
-        }`}
-      >
-        {hit.name}
-      </p>
+        <p
+          className={`my-auto font-medium text-xs transition duration-300 ${
+            isHovered ? "text-blue-500" : ""
+          }`}
+        >
+          {hit.name}
+        </p>
+      </div>
+      <div className="flex">
+        <p
+          className={`inline-flex h-auto my-auto px-2 py-1 text-xs rounded-lg justify-center items-center bg-gray-200 text-gray-500 mr-1`}
+        >
+          {hit.category}
+        </p>
+        <p
+          className={`inline-flex h-auto my-auto px-2 py-1 text-xs rounded-lg justify-center items-center ${
+            hit.force === "push"
+              ? "bg-indigo-100 text-indigo-500"
+              : "bg-blue-100 text-blue-500"
+          }`}
+        >
+          {hit.force}
+        </p>
+      </div>
     </div>
   );
 };
